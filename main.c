dbb60702b2b91101531da0eec7ca33821e846853
John Hinch <johnhnch864@gmail.com>
2024-03-04 11:47:24 +0000

    Initial Commit

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Declare an array to store 5 numbers
+    int numbers[] = {1, 2, 3, 4, 5};
+
+    // Loop through the array and print each number
+    for (int i = 0; i < 5; i++) {
+        printf("%d\n", numbers[i]);
+    }
+
+    return 0;
+}
